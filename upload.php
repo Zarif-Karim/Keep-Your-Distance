4cd51fb7e8b584a076a925fb1ffcae3a5e10c024
thingockieuluong <81518947+thingockieuluong@users.noreply.github.com>
2021-04-06 01:11:56 +1000

    all file updated

--- upload.php
+++ upload.php
@@ -1,38 +1,66 @@
-<?php
-	if (isset($_POST['submit'])){
-		$file = $_FILES['file'];
-		
-		$fileName = $_FILES['file']['name'];
-		$fileTmpName = $_FILES['file']['tmp_name'];
-		$fileSize = $_FILES['file']['size'];
-		$fileError = $_FILES['file']['error'];
-		$fileType = $_FILES['file']['type'];
+	<html>
+	 <head>
+	<title>Upload File</title>
+	</head>
+	 <body bgcolor="pink" color="yellow">
+	 <center>
+	 <h3>File uploaded !! <h3>
+	 <hr>
+	 <?php
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["file"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	$file_save_name = $target_dir . date("d.m.Y h-i-s A") . "." . $fileType;
 
-		$fileExt = explode('.', $fileName);
-		$fileActualExt = strtolower(end($fileExt));
-		
-		$allowed = array('jpg', 'jpeg', 'png', 'pdf');
-		
-		if (in_array($fileActualExt, $allowed)){
-			if($fileError === 0){
-				if ($fileSize < 1000000){
-					$fileNameNew = uniqid('', true).".".$fileActualExt;
-					$fileDistination = "uploads/".$fileNameNew;
-					move_uploaded_file($fileTmpName, $fileDistination);
-					header("Location:ImageProcessing.html?uploadedsuccess");
-				} 
-				else 
-				{
-				echo "Your file is too big!";
-			    } 
-			}
-			else {
-			echo "There was an error uploading your file!";
-
-		}
-		}
-		else 
-		{
-			echo "You cannnot upload files of this type!";
+	// Check if image file is a actual image or fake image
+	if(isset($_POST["submit"])) {
+		$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+		if($check !== false) {
+			echo "File is an image - " . $check["mime"] . ".";
+			$uploadOk = 1;
+		} else {
+			echo "File is not an image.";
+			$uploadOk = 0;
 		}
 	}
+
+	// Check if file already exists
+	if (file_exists($target_file)) {
+		echo "Sorry, file already exists.";
+		$uploadOk = 0;
+	}
+
+	// Check file size
+	if ($_FILES["file"]["size"] > 500000) {
+		echo "Sorry, your file is too large.";
+		$uploadOk = 0;
+	}
+
+	// Allow certain file formats
+	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+	&& $imageFileType != "gif" ) {
+		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		$uploadOk = 0;
+	}
+
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+		echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+		if (move_uploaded_file($_FILES["file"]["tmp_name"], $file_save_name)) {
+			echo "The file ". htmlspecialchars( basename( $_FILES["file"]["name"])). " has been uploaded.";
+		} else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+	}
+	?>
+
+	 <form action = "printPost.cgi" method = "post">
+		 <input type="text" value="<?php echo $file_save_name ?>" name="userfilename"/>
+		 <input type = "submit" name = "submit"/>
+	 </form>
+	 </center>
+	 </body>
+	 </html>
