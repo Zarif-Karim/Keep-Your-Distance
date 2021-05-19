1e789fa6822e160ab154ebc905348d60ebc3be80
thingockieuluong <81518947+thingockieuluong@users.noreply.github.com>
2021-05-19 14:40:58 +1000

    changed checkboxes to drop down and try d3.js for graph

---
+++ plot.js
@@ -0,0 +1,11 @@
+// Set SVG width, height, and padding
+const w = 500;
+const h = 500;
+const padding = 60;
+d3.csv('live-camera.csv', function (data) {
+return [
+	+d['Wavelength'],
+	+d['Sample_1_Absorbance'],
+	+d['Sample_2_Absorbance']
+]
+}).then(plot_data);
